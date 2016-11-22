ae3d315d75cce4a7289cf329eb8f228fddb5754b
naresh1318 <rnaresh.n@gmail.com>
2016-11-22 14:17:40 +0530

    Initial Commit

---
+++ Decision_Tree_test_range.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+import numpy as np
+import pandas as pd
+import pydotplus
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+# Parameters
+top_feat_no = 10
+
+print("Loading data...")
+
+# loading the data set
+df = pd.read_csv('LABTESTS_DEATH_V2.csv', header=None)
+X = df.iloc[1:, 1:-2].values
+Y = df.iloc[1:, -1].values
+Y = Y.astype(np.float)
+
+# Split train test
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
+
+print("Data loaded!!!")
+
+# Used to get the feature labels from the DataFrame
+feat_labels = df.columns[1:-2]
+# Train RandomForest Classifier to know the feature importance
+forest = RandomForestClassifier(n_estimators=10, random_state=0, n_jobs=-1)
+forest.fit(X_train, Y_train)
+# Get the feature importance from the RandomForestClassifier, has a high value for important features
+importance = forest.feature_importances_
+# Get the indices of the important features, 1 is added because the indices start at 0
+indices = np.argsort(importance) + 1
+
+# Select the best indices columns from the DataFrame,
+# last top_feat_no are chosen since the indices are sorted in the ascending order
+df = df[indices[-top_feat_no:]]
+# Get only the test values from the DataFrame
+X2 = df.iloc[1:, :].values
+# Copy the target list
+Y2 = Y[:]
+
+print("Features Selected!!!")
+
+# Split the data into training and testing sets
+X_train2, X_test2, Y_train2, Y_test2 = train_test_split(X2, Y2, test_size=0.1, random_state=0)
+
+# Use Decision Tree Classifier to get the graph, which can be used to get the threshold test values
+clf = DecisionTreeClassifier(max_depth=4)
+clf.fit(X_train2, Y_train2)
+clf_pred = clf.predict(X_test2)
+
+# Get the accuracy for Decision Tree
+print("%.2f%%" % (accuracy_score(clf_pred, Y_test2) * 100))
+
+# Export the graph
+with open("Lab_Tests.dot", 'w') as f:
+    f = tree.export_graphviz(clf, out_file=f)
+
